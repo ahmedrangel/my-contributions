d05f5a7c06fe2b93140efc68021a98ddddd6d8b6
Ahmed Rangel <ahmedrangel@outlook.com>
2024-10-23 17:59:27 -0500

    chore: add icons for each PR state

--- app/components/PullRequest.vue
+++ app/components/PullRequest.vue
@@ -9,6 +9,16 @@ function formatStars (stars: number) {
     maximumFractionDigits: 1
   }).format(stars);
 }
+
+function mapIcons (state: PullRequest["state"]) {
+  switch (state) {
+    case "open": return "i-ph-git-pull-request-duotone";
+    case "merged": return "i-ph-git-merge-duotone";
+    case "draft": return "i-hugeicons-git-pull-request-draft";
+    case "closed": return "i-hugeicons-git-pull-request-closed";
+    default: return "i-ph-git-pull-request-duotone";
+  }
+}
 </script>
 
 <template>
@@ -26,11 +36,12 @@ function formatStars (stars: number) {
       <div class="flex flex-col min-w-0 gap-0.5 sm:gap-1">
         <a :href="data.url" target="_blank" class="text-sm sm:text-base flex items-center gap-0.5 sm:gap-1 hover:underline text-gray-900 dark:text-white">
           <UIcon
-            name="i-ph-git-pull-request-duotone"
+            :name="mapIcons(data.state)"
             :class="{
               'text-green-500 dark:text-green-400': data.state === 'open',
               'text-gray-500 dark:text-gray-400': data.state === 'draft',
               'text-purple-500 dark:text-purple-400': data.state === 'merged',
+              'text-red-500 dark:text-red-400': data.state === 'closed',
             }"
             class="size-4 sm:size-5 shrink-0"
           />
