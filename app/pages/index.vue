861866f25b05ee70df41e6c7b1e6e306fe542caf
Ahmed Rangel <ahmedrangel@outlook.com>
2024-10-25 03:28:39 -0500

    chore: use <main> in pages

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -28,72 +28,74 @@ useSeoMeta({
 </script>
 
 <template>
-  <UContainer class="p-4 sm:p-6 lg:p-8 lg:pt-10 max-w-3xl">
-    <div class="flex flex-col items-center gap-2">
-      <a :href="userUrl" target="_blank">
-        <UAvatar
-          :src="user.avatar"
-          :alt="user.name"
-          size="xl"
-        />
-      </a>
-      <h1 class="text-2xl sm:text-3xl text-center">
+  <main>
+    <UContainer class="p-4 sm:p-6 lg:p-8 lg:pt-10 max-w-3xl">
+      <div class="flex flex-col items-center gap-2">
         <a :href="userUrl" target="_blank">
-          {{ user.name }}
+          <UAvatar
+            :src="user.avatar"
+            :alt="user.name"
+            size="xl"
+          />
         </a>
-        is <span class="animate-pulse">Contributing...</span>
-      </h1>
-      <p class="text-center text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300">
-        <NuxtLink :to="userUrl" target="_blank">
-          @{{ user.username }}'s recent contributions on GitHub.
-        </NuxtLink>
-      </p>
-      <div class="flex items-center justify-center gap-1 text-gray-700 dark:text-gray-300">
-        <ClientOnly>
+        <h1 class="text-2xl sm:text-3xl text-center">
+          <a :href="userUrl" target="_blank">
+            {{ user.name }}
+          </a>
+          is <span class="animate-pulse">Contributing...</span>
+        </h1>
+        <p class="text-center text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300">
+          <NuxtLink :to="userUrl" target="_blank">
+            @{{ user.username }}'s recent contributions on GitHub.
+          </NuxtLink>
+        </p>
+        <div class="flex items-center justify-center gap-1 text-gray-700 dark:text-gray-300">
+          <ClientOnly>
+            <UButton
+              :aria-label="`${user.name}'s GitHub profile`"
+              :icon="colorMode.value === 'dark' ? 'i-ph-moon-stars-duotone' : 'i-ph-sun-duotone'"
+              color="gray"
+              variant="link"
+              @click="colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'"
+            />
+            <template #fallback>
+              <div class="w-8 h-8" />
+            </template>
+          </ClientOnly>
           <UButton
+            :to="userUrl"
+            external
+            target="_blank"
             :aria-label="`${user.name}'s GitHub profile`"
-            :icon="colorMode.value === 'dark' ? 'i-ph-moon-stars-duotone' : 'i-ph-sun-duotone'"
+            icon="i-ph-github-logo-duotone"
             color="gray"
             variant="link"
-            @click="colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'"
           />
-          <template #fallback>
-            <div class="w-8 h-8" />
-          </template>
-        </ClientOnly>
-        <UButton
-          :to="userUrl"
-          external
-          target="_blank"
-          :aria-label="`${user.name}'s GitHub profile`"
-          icon="i-ph-github-logo-duotone"
-          color="gray"
-          variant="link"
-        />
-        <UButton
-          to="/feed.xml"
-          external
-          target="_blank"
-          aria-label="RSS Feed"
-          icon="i-ph-rss-simple-duotone"
-          color="gray"
-          variant="link"
-        />
-      </div>
-      <UDivider class="sm:mt-6 mb-6 w-1/2 mx-auto animate-pulse" />
-    </div>
-
-    <UTabs :items="[{ key: 'prs', label: 'Pull Requests' }, { key: 'iss', label: 'Issues' }]">
-      <template #item="{ item }">
-        <div class="mt-6">
-          <div v-if="item.key === 'prs'" class="flex flex-col gap-6 sm:gap-6">
-            <ContributionPullRequest v-for="pr of prs" :key="pr.url" :data="pr" />
-          </div>
-          <div v-if="item.key === 'iss'" class="flex flex-col gap-6 sm:gap-6">
-            <ContributionIssues v-for="issue of issues" :key="issue.url" :data="issue" />
-          </div>
+          <UButton
+            to="/feed.xml"
+            external
+            target="_blank"
+            aria-label="RSS Feed"
+            icon="i-ph-rss-simple-duotone"
+            color="gray"
+            variant="link"
+          />
         </div>
-      </template>
-    </UTabs>
-  </UContainer>
+        <UDivider class="sm:mt-6 mb-6 w-1/2 mx-auto animate-pulse" />
+      </div>
+
+      <UTabs :items="[{ key: 'prs', label: 'Pull Requests' }, { key: 'iss', label: 'Issues' }]">
+        <template #item="{ item }">
+          <div class="mt-6">
+            <div v-if="item.key === 'prs'" class="flex flex-col gap-6 sm:gap-6">
+              <ContributionPullRequest v-for="pr of prs" :key="pr.url" :data="pr" />
+            </div>
+            <div v-if="item.key === 'iss'" class="flex flex-col gap-6 sm:gap-6">
+              <ContributionIssues v-for="issue of issues" :key="issue.url" :data="issue" />
+            </div>
+          </div>
+        </template>
+      </UTabs>
+    </UContainer>
+  </main>
 </template>
